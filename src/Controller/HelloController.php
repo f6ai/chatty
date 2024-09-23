d0ea1d3350511b86b58c4f2f74a1a1646fbb9d96
Fanni Szikszai <fanniszikszai@gmail.com>
2024-09-23 13:33:31 +0200

    initial commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HelloController extends AbstractController
+{
+    private array $messages = ['Hello', 'Hi', 'World'];
+
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return new Response(implode(',', $this->messages));
+    }
+
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+    public function showOne($id): Response 
+    {
+        return new Response($this->messages[$id]);
+
+    }
+}
